dfe49dc28b948fda88ff0cf1061a3ab85088c256
Ankit Sinha <ankitbko@gmail.com>
2019-07-28 21:59:44 +0530

    Added sharecontroller changed route scheme

---
+++ src/core/SimplyShare.Core/Models/PrimarySeeder.cs
@@ -0,0 +1,46 @@
+﻿using SimplyShare.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SimplyShare.Core
+{
+    public class PrimarySeeder
+    {
+        public string Id { get; set; }
+
+        public MetaInfo MetaInfo { get; set; }
+
+        public List<Address> Addresses { get; set; }
+
+    }
+
+    public class Address
+    {
+        private IPAddress _host;
+
+        public AddressType Type { get; set; }
+
+        public string Host
+        {
+            get
+            {
+                return _host?.ToString();
+            }
+            set
+            {
+                _host = IPAddress.Parse(value);
+            }
+        }
+
+        public int Port { get; set; }
+    }
+
+    public enum AddressType
+    {
+        Public,
+        Internal
+    }
+}
--- src/tracker/SimplyShare.Tracker/Controllers/ShareController.cs
+++ src/tracker/SimplyShare.Tracker/Controllers/ShareController.cs
@@ -5,18 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SimplyShare.Core;
-using SimplyShare.Core.Models;
 
 namespace SimplyShare.Tracker.Controllers
 {
-    [Route("api/announce")]
+    [Route("api/[controller]")]
     [ApiController]
-    public class AnnounceController : ControllerBase
+    public class ShareController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<AnnounceResponse> Get([FromQuery] AnnounceRequest request)
+        [HttpPost]
+        public ActionResult StartSharing(PrimarySeeder request)
         {
-            return new AnnounceResponse();
+            return NotFound();
         }
     }
 }
